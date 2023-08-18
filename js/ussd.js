1956d4bbf2f9c5f9a7200a5b33e670524c5699f3
Prosper <anosikeprosper2@gmail.com>
2023-08-18 07:00:52 +0100

    new:updated uss.js file

--- js/ussd.js
+++ js/ussd.js
@@ -1,5 +1,5 @@
 
-const yourName= prompt(`What is your name?`)
+const yourName= prompt(`What is your name?`) || 'Siri'
 alert(`Hello ${yourName}. Welcome to UBA`)
 const selectOption = prompt(`Please Select an option
 1.Airtime-self
@@ -8,11 +8,10 @@ const selectOption = prompt(`Please Select an option
 4.Transfer-Other Banks
 `)
 
-// console.log(typeof selectOption)
 
 if(selectOption === '1'){
     const enterPin = prompt('Please enter your PIN')
-    // console.log(enterPin.length)
+    
     if (enterPin.length !== 4){
         alert('Invalid pin. Pin must be four numbers')
     }else{
@@ -55,7 +54,7 @@ if(selectOption === '1'){
     const enterPin = prompt('Please enter your PIN')
 
     if (enterPin.length !== 4){
-        alert('Invalid pin. Please enter a valid pin')
+        alert('Invalid pin. Pin must be four numbers')
     }else{
 
         let acctNum= prompt('Please enter your account number')
@@ -78,39 +77,43 @@ if(selectOption === '1'){
 }else if(selectOption === '4'){
 
 
-
-
-
     const enterPin = prompt('Please enter your PIN')
 
-    // if (enterPin.length !== 4){
-    //     alert('Invalid pin. Please enter a valid pin')
-    // }else{
+    if (enterPin.length !==4){
+        alert(`Invalid pin. Pin must be four numbers`)
+    }else{
+        const selectBank= prompt(`Please Select a Bank
+        1. ZENITH BANK
+        2.GTB BANK
+        3.FIRST BANK
+        4.STERLING BANK
+        5.ECO BANK
+        `)
 
-    //     let acctNum= prompt('Please enter your account number')
+        if (!selectBank){
+            
+            alert(`Invalid Request. Please select a bank from the list of options`)
+        }else{
+
+            const acctNum= prompt('Please enter your account number')
         
-    //     if(acctNum.length !== 10 || acctNum.length > 10){
-    //         alert('Invalid account number. Account Number must be 11 numbers')
-    //     }else{
+        if(acctNum.length !== 10 || acctNum.length > 10){
+            alert('Invalid account number. Account Number must be 11 numbers')
+        }else{
 
-    //         const entAmt= prompt('Please enter an amount')
-    //         if(!entAmt || entAmt <=0){
-    //             alert('Invalid amount. Amount must be greater than zero')
-    //         }else{
-    //             alert(`Successful transferred ${entAmt} to ${acctNum}`)
-    //         }
-    //     }
+            const entAmt= prompt('Please enter an amount')
+            if(!entAmt || entAmt <=0){
+                alert('Invalid amount. Amount must be greater than zero')
+            }else{
+                alert(`Successful transferred ${entAmt} to ${acctNum}`)
+            }
+        }
+            
+        }
+    }
 
+    
 
-
-
-
-
-
-
-
-
-
-
-
+}else{
+    alert('Invalid option. Select a valid option from the list provided.')
 }
